6859d6c2b3e269db4e82bf7bd314ab062617e69f
andres gomez <161092979+andrego151k@users.noreply.github.com>
2024-03-11 21:14:03 -0500

    Create p09_par.py

---
+++ p09_par.py
@@ -0,0 +1,8 @@
+#  valor numérico
+valor = int(input("Ingrese un valor numérico: "))
+
+# condicional par
+if valor % 2 == 0:
+    print("El valor ingresado es par.")
+else:
+    print("El valor ingresado no es par.")
